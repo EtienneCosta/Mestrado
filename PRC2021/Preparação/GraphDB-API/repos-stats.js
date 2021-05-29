069e02b6f29cc14a4296c7565b93626e7d9c8933
Etienne Costa <etienne_costa@hotmail.com>
2021-05-29 01:42:41 +0100

    More SPARQL Stuff

--- PRC2021/Preparação/GraphDB-API/repos-stats.js
+++ PRC2021/Preparação/GraphDB-API/repos-stats.js
@@ -1,6 +1,6 @@
 var axios= require('axios')
 
-axios.get('http://localhost:7200/rest/repositories/Slate2020/size')
+axios.get('http://localhost:7200/rest/repositories/ufc/size')
     .then (dados => {
     console.dir(dados.data)
     })
